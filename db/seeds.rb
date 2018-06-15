b6c764383630f8deebfcd3912531ea16c5ae4d48
ran010 <ranjanbajra@gmail.com>
2018-06-15 22:09:55 +0545

    change for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(email:"admin@test.com", isAdmin:true, password:"aaaaaa", password_confirmation:"aaaaaa", address:"Gwarko")
-
-User.create(email:"abc@test.com", isAdmin:false, password:"aaaaaa", password_confirmation:"aaaaaa", address:"patan",batch:"072",semester:"5th")
-User.create(email:"abc@test.com", isAdmin:false, password:"aaaaaa", password_confirmation:"aaaaaa", address:"kathmandu",batch:"071",semester:"7th")
+# 
+# User.create(email:"admin@test.com", isAdmin:true, password:"aaaaaa", password_confirmation:"aaaaaa", address:"Gwarko")
+#
+# User.create(email:"abc@test.com", isAdmin:false, password:"aaaaaa", password_confirmation:"aaaaaa", address:"patan",batch:"072",semester:"5th")
+# User.create(email:"abc@test.com", isAdmin:false, password:"aaaaaa", password_confirmation:"aaaaaa", address:"kathmandu",batch:"071",semester:"7th")
